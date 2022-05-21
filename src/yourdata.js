1c5cab944d84dca0553b23f6fa20aa25cb6d8a3a
ammar-8651 <85989347+ammar-8651@users.noreply.github.com>
2022-05-21 22:02:06 +0500

    Fixed Xana URL

--- src/yourdata.js
+++ src/yourdata.js
@@ -120,7 +120,7 @@ export default {
       //"/images/work/eboh.jpg",
        // "https://images.unsplash.com/photo-1492171983775-a51717616c0d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8NjB8fHBhc3RlbHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=400&q=60",
       //Project URL - Add Your Project Url Here
-      url: "https://www.xana.net/home",
+      url: "https://www.xana.net/",
     },
     {
       id: 6, //DO NOT CHANGE THIS (Please)😅
